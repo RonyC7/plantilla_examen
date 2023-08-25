e159a5e4c47b6406b47a47f4aaa0cb6db492ff75
RonyC7 <79855001+RonyC7@users.noreply.github.com>
2023-08-24 22:57:10 -0600

    Examen terminado, agregando imagenes utilizadas y el diseño terminado

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: ''),
@@ -25,8 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-
 
   final String title;
 
@@ -39,43 +35,89 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         title: Text(widget.title),
       ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Huellitas',
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'assets/ellipse-158.png',
+                  width: 500,
+                  height: 265,
+                ),
+                Positioned(
+                  top: 0,
+                  child: Image.asset(
+                    'assets/whatsappimage2023-08-10at201935-transformed-1.png',
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _incrementCounter,
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 40),
             ),
-            ElevatedButton(onPressed: _incrementCounter, child: Text('Continuar con google'))
-
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-
-
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 30,
+                  height: 30,
+                  child: Image.asset('assets/image-1-XqZ.png'),
+                ),
+                SizedBox(width: 3),
+                Text('Continuar con Google'),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size(double.infinity, 40),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 30,
+                  height: 30,
+                  child: Image.asset('assets/image-1.png'),
+                ),
+                SizedBox(width: 3),
+                Text('Solo ver'),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            margin: EdgeInsets.only(bottom: 20),
+            child: Image.asset(
+              'assets/ellipse-159.png',
+              width: 400,
+              height: 242,
+            ),
+          ),
+        ],
       ),
     );
   }
